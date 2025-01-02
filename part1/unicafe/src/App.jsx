cf2084ca27a74650050a430fdcbe4eab66ca16d5
Laura Kauria <laura.kauria@helsinki.fi>
2025-01-02 14:29:03 +0200

    Preliminary structure, unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,64 @@
 import { useState } from "react";
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        Not given feedback yet
+      </div>
+    )
+  }
+  return (
+    <div>
+      {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
+const Button = (props) => { 
+
+  console.log(props)
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
+  const handleGoodClick = () => {
+
+  }
+  
+  const handleNeutralClick = () => {
+
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(updatedRight + left)
+  const handleBadClick = () => {
+
   }
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
+      Give feedback
 
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <History allClicks={allClicks} /> 
+      {/* //not as a text here but an own component\ */}
+      Good, {good}
+      Neutral, {neutral}
+      Bad, {bad}
       <p>total {total}</p>
     </div>
   )
